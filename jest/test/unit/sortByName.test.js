a1e21f085a8c615241bce335f310b1bf55c5a98d
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2024-06-23 13:51:43 +0300

    runing project HW_7.3

---
+++ jest/test/unit/sortByName.test.js
@@ -0,0 +1,18 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+    const output = sorting.sortByName(input);
+    expect(output).toEqual(expected);
+  });
+});
